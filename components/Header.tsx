1992eb3add8973b8f1af3707b146cbfddf89d657
hereskira <shakiraregalado@gmail.com>
2024-08-10 15:33:23 +0800

    Add working confirmation email

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { twMerge } from "tailwind-merge";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -16,6 +17,7 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className
 }) => {
+    const authModal = useAuthModal();
     const router = useRouter();
 
     const handleLogout = () => {
@@ -43,12 +45,12 @@ const Header: React.FC<HeaderProps> = ({
                 <div className="flex justify-between items-center gap-x-4">
                     <>
                     <div>
-                        <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">
+                        <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">
                             Sign up
                         </Button>
                     </div>
                     <div>
-                        <Button onClick={() => {}} className="bg-white px-6 py-2">
+                        <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
                             Log in
                         </Button>
                     </div>
