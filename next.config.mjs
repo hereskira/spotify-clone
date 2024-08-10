0e3bd02d577faa649189f316d95d491b7f52f3d5
hereskira <shakiraregalado@gmail.com>
2024-08-10 22:50:06 +0800

    Fetch the image/song at the homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "vffjvuycxpfgpbfzylmh.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
